652fe308147785e006da35189cd2c631c3c176cb
Oleg Novoselskyi <143404411+OlegNovoselskij@users.noreply.github.com>
2025-05-08 18:38:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
-import adminRouter from './routes/adminRoute.js';
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import adminRouter from "./routes/adminRoute.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,13 +18,13 @@ connectDB();
 
 // API Endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
+app.use("/images", express.static("uploads"));
 app.use("/api/admin", adminRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Working");
+  res.send("API Working");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
