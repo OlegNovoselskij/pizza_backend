2d6dc31f97511a345db9a6b101a7615dc6a60a52
Oleg Novoselskyi <143404411+OlegNovoselskij@users.noreply.github.com>
2025-05-08 18:38:52 +0300

    Update foodRoute.js

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,21 +1,19 @@
-import express from 'express';
-import multer from 'multer';
-import { addFood, listFood, removeFoodItem } from '../controllers/foodController.js';
+import express from "express";
+import multer from "multer";
+import {
+  addFood,
+  listFood,
+  removeFoodItem,
+} from "../controllers/foodController.js";
 
 const foodRouter = express.Router();
 
-// Image Storage Engine
-const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`);
-    }
-});
+// Зберігаємо файл в пам'яті (а не на диск)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const upload = multer({ storage: storage });
-
-foodRouter.post('/add', upload.single("image"), addFood);
-foodRouter.get('/list', listFood);
-foodRouter.delete('/remove', removeFoodItem);
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.delete("/remove", removeFoodItem);
 
 export default foodRouter;
